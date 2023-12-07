37194c07d29fc36495faa554bd17b3c5437f90c0
pjh5474 <warwarsn@gmail.com>
2023-12-07 15:45:34 +0900

    Chain

--- pages/01_DocumentGPT.py
+++ pages/01_DocumentGPT.py
@@ -13,7 +13,10 @@ st.set_page_config(
     page_icon="🐼",
 )
 
+llm = ChatOpenAI(temperature=0.1)
 
+
+@st.cache_data(show_spinner="Embedding file...")
 def embed_file(file):
     file_content = file.read()
     file_path = f"./.cache/files/{file.name}"
@@ -26,7 +29,7 @@ def embed_file(file):
         chunk_size=600,
         chunk_overlap=100,
     )
-    loader = UnstructuredFileLoader("./files/chapter_one.docx")
+    loader = UnstructuredFileLoader(file_path)
     docs = loader.load_and_split(text_splitter=splitter)
     embeddings = OpenAIEmbeddings()
     cached_embeddings = CacheBackedEmbeddings.from_bytes_store(embeddings, cache_dir)
@@ -34,6 +37,48 @@ def embed_file(file):
     retriever = vectorstore.as_retriever()
     return retriever
 
+
+def send_message(message, role, save=True):
+    with st.chat_message(role):
+        st.markdown(message)
+    if save:
+        st.session_state["message"].append(
+            {
+                "message": message,
+                "role": role,
+            }
+        )
+
+
+def paint_history():
+    for message in st.session_state["messages"]:
+        send_message(
+            message["message"],
+            message["role"],
+            save=False,
+        )
+
+
+def format_docs(docs):
+    return "\n\n".join(document.page_content for document in docs)
+
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            """
+            Answer the question using ONLY the following context. If you don't know the answer just say you don't know. DON'T make anything up.
+     
+            Context: {context}
+            """,
+        ),
+        (
+            "human",
+            "{question}",
+        ),
+    ]
+)
 
 st.title("Chowis - DocumentGPT")
 
@@ -44,10 +89,33 @@ st.markdown(
     """
 )
 
-file = st.file_uploader(
-    "Upload a .txt, .pdf or .docx file",
-    type=["pdf", "txt", "docx"],
-)
+with st.sidebar:
+    file = st.file_uploader(
+        "Upload a .txt, .pdf or .docx file",
+        type=["pdf", "txt", "docx"],
+    )
 
 if file:
     retriever = embed_file(file)
+    send_message(
+        "I'm ready! Ask away!",
+        "ai",
+        save=False,
+    )
+    paint_history()
+    message = st.chat_input("Ask anything about your file")
+    if message:
+        send_message(message, "human")
+        chain = (
+            {
+                "context": retriever | RunnableLambda(format_docs),
+                "question": RunnablePassthrough(),
+            }
+            | prompt
+            | llm
+        )
+        response = chain.invoke(message)
+        send_message(response.content, "ai")
+
+else:
+    st.session_state["messages"] = []
